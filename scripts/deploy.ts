af508f9c0d6ff8728f5101326159b10d9c68aab6
blacksecrettoken <secretsuperdev9472@gmail.com>
2023-08-01 23:38:03 -0700

    finish contract

---
+++ scripts/deploy.ts
@@ -0,0 +1,23 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const unlockTime = currentTimestampInSeconds + 60;
+
+  // const lockedAmount = ethers.parseEther("0.001");
+
+  const token = await ethers.deployContract("Token");
+
+  await token.waitForDeployment();
+
+  console.log(
+    `deployed to ${token.target}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
